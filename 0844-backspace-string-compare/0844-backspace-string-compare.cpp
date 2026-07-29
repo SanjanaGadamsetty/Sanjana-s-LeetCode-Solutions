f949420717fc3de9f59ecb6006f9c7d1d78b8095
Gadamsetty Venkata Nagalakshmi Sanjana <gvnsanjana.ds@gmail.com>
2026-07-29 14:34:53 +0530

    Time: 0 ms (100%), Space: 8.8 MB (13.42%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -13,14 +13,6 @@ public:
             else if (c!= '#') s2.push(c);
         }
 
-        if (s1.size()!= s2.size()) return false;
-
-        while (!s1.empty() && !s2.empty()){
-            if (s1.top()!= s2.top()) return false;
-            s1.pop();
-            s2.pop();
-        }
-
-        return true;
+        return s1== s2;
     }
 };
